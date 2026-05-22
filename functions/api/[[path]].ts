74afb64b7858254f06dcbc526d4315f58d4e7ae4
engineerdanielrunyowa <engineerdanielrunyowa@gmail.com>
2026-05-22 14:18:46 +0200

    Update [[path]].ts

--- functions/api/[[path]].ts
+++ functions/api/[[path]].ts
@@ -2,7 +2,7 @@ export async function onRequest(context: any) {
   const url = new URL(context.request.url);
 
   const workerUrl =
-    `https://mining-portfolio-worker.engineerdaniel.workers.dev${url.pathname}${url.search}`;
+    `https://portfolio-api.engineerdaniel.workers.dev${url.pathname}${url.search}`;
 
   return fetch(workerUrl, context.request);
 }
